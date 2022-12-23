c26bf6d711c875cb347679631a2582f570f37dad
Andhitia Rama <andhitia.r@gmail.com>
2022-12-24 05:01:51 +0700

    [ssi_appointment] 14.0.1.0.0

---
+++ ssi_appointment/__manifest__.py
@@ -0,0 +1,38 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Appointment",
+    "version": "14.0.1.1.2",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_master_data_mixin",
+        "ssi_transaction_confirm_mixin",
+        "ssi_transaction_open_mixin",
+        "ssi_transaction_done_mixin",
+        "ssi_transaction_cancel_mixin",
+        "ssi_transaction_terminate_mixin",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/approval_template_data.xml",
+        "menu.xml",
+        "views/appointment_type_views.xml",
+        "views/appointment_time_slot_views.xml",
+        "views/appointment_schedule_views.xml",
+    ],
+    "demo": [
+        "demo/appointment_time_slot_demo.xml",
+        "demo/appointment_type_demo.xml",
+    ],
+}
